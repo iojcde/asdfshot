9a6a58acc4ccdb2cb7488dafd95b93601bc0f356
JcdeA <io@fosshost.org>
2022-09-23 22:40:33 +0000

    Revert "use puppeteer"
    This reverts commit 01b9c0616f611b88b3b13ff8f8e26c56dfa606f7.

--- pages/api/shot.ts
+++ pages/api/shot.ts
@@ -1,7 +1,9 @@
 import { unstable_getServerSession } from "next-auth/next"
 import type { NextApiRequest, NextApiResponse } from "next"
-const chromium = require('chrome-aws-lambda')
+import chrome from 'chrome-aws-lambda'
 import { authOptions } from "./auth/[...nextauth]"
+
+import { chromium } from "playwright"
 
 const shot = async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await unstable_getServerSession(req, res, authOptions)
@@ -9,28 +11,16 @@ const shot = async (req: NextApiRequest, res: NextApiResponse) => {
     if (session.user?.email !='io@fosshost.org'){
       res.status(401)
     }
-    
-   const browser = await chromium.puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless,
-      ignoreHTTPSErrors: true,
+    const browser = await chromium.launch({
+      args: chrome.args,
+      executablePath: await chrome.executablePath,
+      headless: chrome.headless,
     });
-
-    // Create a new page
-    const page = await browser.newPage();
-
-  // Set viewport width and height
-    await page.setViewport({ width: 1440, height: 770 });
-
-    const { url } = req.query
-
-  // Open URL in current page
-  await page.goto(url as string, { waitUntil: 'networkidle0' });
-
-  // Capture screenshot
-  const buffer = await page.screenshot();
+  const { url } = req.query
+    let page = await browser.newPage()
+    await page.setViewportSize({ width: 1440, height: 770})
+    await page.goto(url as string).catch(err =>{console.error(err)})
+    const buffer = await page.screenshot({ type:"png",timeout: 8000 })
     res.setHeader("content-type", "image/png")
     res.status(200).write(buffer)
   } else {
